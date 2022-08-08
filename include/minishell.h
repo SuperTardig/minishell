4d62bcdaa34acae96fd70a0ddb197111c8f06e37
Félix-Antoine Leduc <fleduc@student.42quebec.com>
2022-08-08 13:43:35 -0400

    added includes

--- include/minishell.h
+++ include/minishell.h
@@ -6,12 +6,30 @@
 /*   By: bperron <bperron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/03 09:40:39 by bperron           #+#    #+#             */
-/*   Updated: 2022/08/03 09:42:17 by bperron          ###   ########.fr       */
+/*   Updated: 2022/08/08 13:36:56 by fleduc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+
+# include <stdio.h>
+# include <readline/readline.h> //de readline (a verifier)
+# include <readline/history.h> //de readline (a verifier)
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <sys/types.h>
+# include <sys/time.h>
+# include <sys/resource.h>
+# include <sys/ioctl.h>
+# include <signal.h>
+# include <dirent.h>
+# include <string.h>
+# include <termios.h>
+# include <curses.h>
+# include <term.h>
 
 
 #endif MINISHELL_H
