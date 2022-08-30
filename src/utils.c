3139a6548f8ac300d44ea98bb4bfe988d8ccbeca
Benjamin Perron <benjaminperron150@gmail.com>
2022-08-30 13:58:40 -0400

    worked on built in

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: bperron <bperron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/12 10:40:21 by bperron           #+#    #+#             */
-/*   Updated: 2022/08/12 11:26:32 by bperron          ###   ########.fr       */
+/*   Updated: 2022/08/30 11:03:15 by bperron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,22 @@ void	cmd_prompt(void)
 		first_open = 0;
 	}
 	printf("\e[0;29mMinishell >"); //command line
+}
+
+int	check_args(t_vars *vars)
+{
+	int	args;
+	int	i;
+
+	args = 0;
+	i = 0;
+	while (i < vars->cmd_len)
+	{
+		if (vars->cmd[i] == '\0')
+			if (vars->metas[vars->i_meta++] == ' ')
+				if (vars->cmd[i + 1] != '\0')
+					args++;
+		i++;
+	}
+	return (args);
 }
